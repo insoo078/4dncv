4e291f2d5b4fb808e51fb3eb9bc27f88ad9f7634
insoo078 <insoo078@nate.com>
2017-01-16 16:15:34 +0900

    upload test JFrame

---
+++ 4DNucleomeViewer/src/org/kobic/s4dncv/Main.java
@@ -0,0 +1,16 @@
+package org.kobic.s4dncv;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Hello");
+		
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+		frame.setSize(640, 480);
+		frame.setVisible(true);
+	}
+}
